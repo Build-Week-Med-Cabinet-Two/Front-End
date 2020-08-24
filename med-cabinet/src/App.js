2d93beceed138d9c345c23390c602d25dc150bb9
Kevin St.Onge <kevinstonge@gmail.com>
2020-08-24 15:20:06 -0400

    setting up new branch

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -4,7 +4,7 @@ import "./App.scss";
 import HeaderKS from "./components/HeaderKS";
 import RegistrationKS from "./components/RegistrationKS";
 import LoginKS from "./components/LoginKS";
-import UserData from ".components/UserData.js";
+import UserData from "./components/UserData.js";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <HeaderKS />
         <Switch>
           <Route exact path="/">
-            <p>home</p>
+            <UserData />
           </Route>
           <Route path="/register">
             <RegistrationKS />
@@ -29,7 +29,6 @@ function App() {
 
 export default App;
 
-
 // import React from "react";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -42,7 +41,7 @@ export default App;
 //     <Router>
 //       <div className="App">
 //         <Route exact path="/" component={Login} />
-      
+
 //         <PrivateRoute exact path='/protected' component={} />
 //       </div>
 //     </Router>
