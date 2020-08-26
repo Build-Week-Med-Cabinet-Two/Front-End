da87bcd038ca47c2a8997ef1f3a108490f597f77
Kevin St.Onge <kevinstonge@gmail.com>
2020-08-26 13:50:16 -0400

    ...

--- med-cabinet/src/components/Favorites.js
+++ med-cabinet/src/components/Favorites.js
@@ -1,5 +1,6 @@
 import React from "react";
 import StrainCard from "./StrainCard";
+import "./Favorites.scss";
 export default function Favorites(props) {
   return (
     <div className="favoritesContainer">
@@ -8,7 +9,12 @@ export default function Favorites(props) {
         <p>you don't have any saved favorites</p>
       )}
       {props.favorites.map((f) => (
-        <StrainCard key={f.strain} strain={f} favorites={props.favorites} />
+        <StrainCard
+          key={f.strain}
+          strain={f}
+          favorites={props.favorites}
+          setFavorites={props.setFavorites}
+        />
       ))}
     </div>
   );
